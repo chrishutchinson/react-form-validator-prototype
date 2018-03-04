b343f0f037421ee00eafdddf8bbac7e6e0ad07ec
Chris Hutchinson <chris_hutchinson@me.com>
2018-03-04 17:07:50 +0000

    Move into lib directory

--- Form/index.js
+++ Form/index.js
@@ -2,144 +2,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ok, err, all, some, isOK, getErrors } from "@times/data-validator";
-import type { Validator } from "@times/data-validator";
 
-const FORM_CONTEXT = "__form-thing__";
+import ValidatorForm, { TextInput, SelectInput, withForm } from "../lib/index";
 
-// Our wrapper function for creating custom components
-const withForm = Component => {
-  const Wrapper = (props, context) => {
-    const formContext = context[FORM_CONTEXT];
-
-    if (props.name && props.validator)
-      formContext.registerValidator(props.name, props.validator);
-
-    return <Component {...formContext} {...props} />;
-  };
-  Wrapper.contextTypes = {
-    [FORM_CONTEXT]: PropTypes.object.isRequired
-  };
-  return Wrapper;
-};
-
-const TextInput = withForm(({ name, onChange }) => (
-  <input
-    type="text"
-    onChange={e => onChange(name, e.target.value)}
-    name={name}
-  />
+const MyTextarea = withForm(({ name, onChange }) => (
+  <textarea onChange={e => onChange(name, e.target.value)} name={name} />
 ));
 
-const SelectInput = withForm(({ name, children, onChange, defaultValue }) => (
-  <select
-    defaultValue={defaultValue}
-    onChange={e => onChange(name, e.target.value)}
-    name={name}
-  >
-    {children}
-  </select>
-));
-
-const ContentEditable = withForm(({ name, onChange }) => (
-  <div contentEditable={true} onInput={e => onChange(name, e.target.innerHTML)}>
-    Hello, world!
-  </div>
-));
-
-class FormWrapper extends React.Component {
-  static TextInput = TextInput;
-
-  // Setup our context data model
-  static childContextTypes = {
-    [FORM_CONTEXT]: PropTypes.object.isRequired
-  };
-
-  static defaultProps = {
-    compositionReducer: all
-  };
-
-  state = {
-    data: {},
-    isValid: null,
-    validators: []
-  };
-
-  registerFieldValidator = (fieldName, validator) => {
-    const { validators } = this.state;
-    if (validators.hasOwnProperty(fieldName)) return;
-
-    this.setState(({ validators }) => ({
-      validators: {
-        ...validators,
-        [fieldName]: validator
-      }
-    }));
-  };
-
-  handleFieldChange = (fieldName, value) => {
-    const { validator } = this.props;
-
-    this.setState(
-      ({ data }) => ({
-        data: {
-          ...data,
-          [fieldName]: value
-        }
-      }),
-      this.validate
-    );
-  };
-
-  validate = () => {
-    const { compositionReducer } = this.props;
-    const { data, validators } = this.state;
-
-    const result = compositionReducer(Object.values(validators))(data);
-
-    this.setState(() => ({
-      isValid: result
-    }));
-  };
-
-  handleSubmit = e => {
-    const { onSubmit, validator } = this.props;
-    const { data } = this.state;
-    const result = validator(data);
-
-    if (!result.valid) e.preventDefault();
-    else onSubmit(data);
-  };
-
-  // More context setup
-  getChildContext() {
-    const { isValid } = this.state;
-
-    return {
-      [FORM_CONTEXT]: {
-        isValid,
-        onChange: this.handleFieldChange,
-        registerValidator: this.registerFieldValidator
-      }
-    };
-  }
-
-  render() {
-    const { children } = this.props;
-    const { isValid } = this.state;
-
-    return <form onSubmit={this.handleSubmit}>{children(isValid)}</form>;
-  }
-}
-
-type FormProps = {};
-export default (props: FormProps) => (
-  <FormWrapper onSubmit={console.log} compositionReducer={some}>
+export default () => (
+  <ValidatorForm onSubmit={console.log} composer={all}>
     {result => (
       <div>
         <p>Is valid?: {result && isOK(result) ? "Yes" : "No"}</p>
         <p>Errors?: {result && getErrors(result).join(", ")}</p>
 
-        <FormWrapper.TextInput
+        <TextInput
           validator={({ firstName }) =>
             firstName && firstName.length > 2
               ? ok()
@@ -148,7 +25,16 @@ export default (props: FormProps) => (
           name="firstName"
         />
 
-        <FormWrapper.TextInput
+        <MyTextarea
+          validator={({ body }) =>
+            body && body.toLowerCase().startsWith("hello")
+              ? ok()
+              : err(['Body must start with "Hello"'])
+          }
+          name="body"
+        />
+
+        <TextInput
           validator={({ lastName }) =>
             lastName && lastName.length > 2
               ? ok()
@@ -168,15 +54,8 @@ export default (props: FormProps) => (
           <option value="gb">Great Britain</option>
         </SelectInput>
 
-        {/* <ContentEditable
-          validator={({ html }) =>
-            html.startsWith("Hello") ? ok() : err(['Must start with "Hello"'])
-          }
-          name="html"
-        /> */}
-
         <input type="submit" />
       </div>
     )}
-  </FormWrapper>
+  </ValidatorForm>
 );
